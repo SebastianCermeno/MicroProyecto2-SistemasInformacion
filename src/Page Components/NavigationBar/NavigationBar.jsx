0cdf042a7396acd5df00034b63f679348c91d72a
SebastianCermeno <sebastian-cermeno@correo.unimet.edu.ve>
2024-03-09 11:53:21 -0400

    Fully implemented initial Router operations.

--- src/Page Components/NavigationBar/NavigationBar.jsx
+++ src/Page Components/NavigationBar/NavigationBar.jsx
@@ -1,6 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import './NavigationBar.css'
 
 function NavigationBar() {
+    const navigationController = useNavigate()
+    const pageDictionary = new Map([
+        ['1', '/'],
+        ['2', '/self'],
+        ['3', '/clubs']
+    ]);
+
+    function navigate(pageID) {
+        try {
+            console.log("Success")
+            navigationController(pageDictionary.get(pageID))
+        } catch (error) {
+            console.log("Failed")
+        }
+    }
     return(
         <>
             <div className='NavBar'>
@@ -8,13 +24,13 @@ function NavigationBar() {
                     World Link Center
                 </h1>
                 <div className='ButtonGroup'>
-                    <button className='NavigationButton'>
+                    <button className='NavigationButton' onClick={() => navigate('1')}>
                         To Home
                     </button>
-                    <button className='NavigationButton'>
+                    <button className='NavigationButton' onClick={() => navigate('2')}>
                         To Profile
                     </button>
-                    <button className='NavigationButton'>
+                    <button className='NavigationButton' onClick={() => navigate('3')}>
                         To Clubs
                     </button>
                 </div>
