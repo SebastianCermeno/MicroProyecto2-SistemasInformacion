661709fca08e7e0d4f3ec4f953c28d2671c2d8b2
Sebastián Cermeño <122610635+SebastianCermeno@users.noreply.github.com>
2024-03-10 17:33:15 -0400

    Implement Account Control Part 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 import NavigationBar from './Page Components/NavigationBar/NavigationBar.jsx'
+import LogInPage from './Pages/Log In Page/LogInPage.jsx'
+import RegisterPage from './Pages/Register Page/RegisterPage.jsx'
 import HomePage from './Pages/Home Page/HomePage.jsx'
 import ProfilePage from './Pages/ProfilePage/ProfilePage.jsx'
 import ClubsPage from './Pages/Clubs Page/ClubsPage.jsx'
 
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 
 function App() {
   const firebaseConfig = {
@@ -18,14 +21,18 @@ function App() {
     appId: "1:314563214259:web:f717dacfd2e96959fbe9c6",
     measurementId: "G-51XGX9LY5X"
   };
-  const app = initializeApp(firebaseConfig)
+  const app = initializeApp(firebaseConfig);
+
+  const authentication = getAuth(app);
   return (
     <Router>
       <NavigationBar></NavigationBar>
       <Routes>
+        <Route path='/login' element={<LogInPage></LogInPage>}/>
         <Route path='/' element={<HomePage></HomePage>}/>
         <Route path='/self' element={<ProfilePage></ProfilePage>}/>
         <Route path='/clubs' element={<ClubsPage></ClubsPage>}/>
+        <Route path='/register' element={<RegisterPage></RegisterPage>}/>
       </Routes>
     </Router>
   )
--- src/Pages/Log In Page/LogInPage.jsx
+++ src/Pages/Log In Page/LogInPage.jsx
@@ -1,27 +1,28 @@
 import './LogInPage.css'
 
-function LogInPage() {
-return (
-        <form className="form">
-          <h2 className="form__title">Inicia Sesión</h2>
-          <p className="form__paragraph">¿Aún no tienes una cuenta? <a href="#" className="form__link">Entra aquí</a></p>
-    
-          <div className="form__container">
-            <div className="form__group">
-              <input type="text" id="name" className="form__input" placeholder=" " />
-              <label htmlFor="name" className="form__label">Correo Electronico:</label>
-              <span className="form__line"></span>
-            </div>
-            
-            <div className="form__group">
-              <input type="password" id="password" className="form__input" placeholder=" " />
-              <label htmlFor="password" className="form__label">Contraseña:</label>
-              <span className="form__line"></span>
-            </div>
-    
-            <input type="submit" className="form__submit" value="Entrar" />
-          </div>
-        </form>
+function LogInPage(authenticator) {
+    const authManager = authenticator
+    return (
+            <form className="form">
+              <h2 className="form__title">Inicia Sesión</h2>
+              <p className="form__paragraph">¿Aún no tienes una cuenta? <a href="#" className="form__link">Entra aquí</a></p>
+        
+              <div className="form__container">
+                <div className="form__group">
+                  <input type="text" id="name" className="form__input" placeholder=" " />
+                  <label htmlFor="name" className="form__label">Correo Electronico:</label>
+                  <span className="form__line"></span>
+                </div>
+                
+                <div className="form__group">
+                  <input type="password" id="password" className="form__input" placeholder=" " />
+                  <label htmlFor="password" className="form__label">Contraseña:</label>
+                  <span className="form__line"></span>
+                </div>
+        
+                <input type="submit" className="form__submit" value="Entrar" />
+              </div>
+            </form>
       );
 
 }
