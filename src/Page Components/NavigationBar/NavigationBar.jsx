4bbc7d9b4a3a6a027d0a19ce7141128eef3a6105
gabo21 <gabriel.martinez@correo.unimet.edu.ve>
2024-03-09 20:22:50 -0400

    Subiendo nav bar y hosteo fire base

--- src/Page Components/NavigationBar/NavigationBar.jsx
+++ src/Page Components/NavigationBar/NavigationBar.jsx
@@ -1,6 +1,7 @@
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import './NavigationBar.css'
 
+/*
 function NavigationBar() {
     const navigationController = useNavigate()
     const pageDictionary = new Map([
@@ -15,6 +16,7 @@ function NavigationBar() {
         try {
             navigationController(pageDictionary.get(pageID))
         } catch (error) {
+            console.log(error);
         }
     }
     return(
@@ -38,5 +40,42 @@ function NavigationBar() {
         </>
     );
 }
+*/ 
+
+
+
+function NavigationBar() {
+    return (
+        <header>
+            <div className="logo">VideoGames Club</div>
+            <input type="checkbox" id="nav_check" hidden />
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/" className="active">Home</a>
+                    </li>
+                    <li>
+                        <a href="/">Perfil</a>
+                    </li>
+                    <li>
+                        <a href="/">Clubes</a>
+                    </li>
+                    <li>
+                        <a href="/">Juegos</a>
+                    </li>
+                </ul>
+            </nav>
+            <label htmlFor="nav_check" className="hamburger">
+                <div></div>
+                <div></div>
+                <div></div>
+            </label>
+        </header>
+    );
+}
 
 export default NavigationBar
+
+
+
+   
