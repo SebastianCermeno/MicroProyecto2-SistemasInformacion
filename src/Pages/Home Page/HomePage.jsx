e5d9086190ff068f60e9305315cfbb1b7d13c2ef
SebastianCermeno <sebastian-cermeno@correo.unimet.edu.ve>
2024-03-11 09:25:16 -0400

    Implement some actual visuals pt1

--- src/Pages/Home Page/HomePage.jsx
+++ src/Pages/Home Page/HomePage.jsx
@@ -1,8 +1,47 @@
+import './HomePage.css'
+
 function HomePage() {
     return(
-        <h1>
-            Home Page
-        </h1>
+        <>
+            <div className="TitleRegion">
+                <h1 className="TitleText">
+                    ¡Bienvenido!
+                </h1>
+            </div>
+            <div className="Content1">
+                <h2 className='SubTitle1'>
+                    Introducción
+                </h2>
+                <div className='SectionContainer1'>
+                    <div className='FirstContentBody'>
+                        <button>
+                            Ver clubes
+                        </button>
+                        <p>
+                            World Link Center es un sitio donde podrás conectar con gamers de todas partes del mundo.
+                            Aquí te encontrarás grandes grupos de usuarios dedicados a todo tipo de juegos y actividades:
+                            desde compartir obras de arte hechas en Minecraft hasta duras competencias en League of
+                            Legends. ¡Únete!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="Content2">
+                <h2 className='SubTitle2'>
+                    Sé tu mismo
+                </h2>
+                <div className='SecondContentBody'>
+                    <button>
+                        Cambiar Perfil
+                    </button>
+                    <p>
+                        ¡Destácate en la personalización de tu perfil! Díle al mundo quien eres y qué es lo
+                        que amas de jugar videojuegos. Tus intereses te podrán conectar con jugadores en
+                        todo el mundo.
+                    </p>
+                </div>
+            </div>
+        </>
     )
 }
 
